e0d0619b8b939f6f569e15e1be1a7ac6098e46ff
hyperhuman <43915849+hyperhuman@users.noreply.github.com>
2020-02-28 14:16:34 -0500

    Update myls.c

--- myls.c
+++ myls.c
@@ -1,6 +1,51 @@
+#include <stdlib.h>
 #include "myls.h"
 #include <stdio.h>
-int main(){
-   printf("hello there");
-   return 0;
+#include <string.h>
+#include <dirent.h>
+#include "stringout.h"
+
+char* mylsFunction(char* dirname[]){
+    DIR *dir_ptr; //directory stream
+	struct dirent *directp; //hold one entry
+    int size = 0;
+    while((directp = readdir(dirname)) != NULL){
+            size = size + 1;
+        }
+    char* files[size];
+    rewinddir(dir_ptr);
+	if((dir_ptr  = opendir(dirname)) == NULL){
+		printf("Cannot open %s\n", dirname);
+        return dirname;
+	}else{
+        int i = 0;
+		while((directp = readdir(dirname)) != NULL){
+           files[i] = (char*)malloc(sizeof(directp->d_name) + 1);
+           strncpy(files[size], directp->d_name, directp->d_namlen);
+           strcat(files[size], '\0');
+           i = i + 1;
+           if(i == size){
+               break;
+           }
+		}
+		close(dir_ptr);
+	}
+    return files;
+}
+
+int main(int argc, char* argv[]){
+    if(argc <= 0){
+        printf("Error no directory provided.");
+        exit;
+    }
+    if(argc == 1){
+        stringout(myls(argv[0]), '\0');
+    }
+    if(argc == 2){
+        stringout(myls(argv[0]), argv[1]);
+    }
+    if(argc > 2){
+        printf("Too many arguments given.");
+        exit;
+    }
 }
